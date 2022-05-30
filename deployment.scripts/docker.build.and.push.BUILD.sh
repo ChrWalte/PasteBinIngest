6157db56e0e228c789dd9e45ef2ce84227e1b850
Christopher Walters <chrwalte@protonmail.com>
2022-05-29 19:58:05 -0700

    updated scripts

--- deployment.scripts/docker.build.and.push.BUILD.sh
+++ deployment.scripts/docker.build.and.push.BUILD.sh
@@ -2,7 +2,7 @@
 # description of script
 
 # exit when any command fails
-set -e
+set -ev
 
 # variables
 # docker hub profile
@@ -14,33 +14,24 @@ VERSION=$(cat ../VERSION)build
 
 # BUILD
 # cmd
-echo "[CMD]: docker build --no-cache -t $PROJECT.cmd:$VERSION -f ./Dockerfile.cmd .."
 docker build --no-cache -t $PROJECT.cmd:$VERSION -f ../Dockerfile.cmd ..
 # api
-echo "[CMD]: docker build --no-cache -t $PROJECT.api:$VERSION -f ./Dockerfile.api .."
 docker build --no-cache -t $PROJECT.api:$VERSION -f ../Dockerfile.api ..
 
 # TAG
 # cmd
-echo "[CMD]: docker tag $PROJECT.cmd:$VERSION $PROFILE/$PROJECT.cmd:$VERSION"
 docker tag $PROJECT.cmd:$VERSION $PROFILE/$PROJECT.cmd:$VERSION
-echo "[CMD]: docker tag $PROJECT.cmd:$VERSION $PROFILE/$PROJECT.cmd:build"
 docker tag $PROJECT.cmd:$VERSION $PROFILE/$PROJECT.cmd:build
 # api
-echo "[CMD]: docker tag $PROJECT.api:$VERSION $PROFILE/$PROJECT.api:$VERSION"
 docker tag $PROJECT.api:$VERSION $PROFILE/$PROJECT.api:$VERSION
-echo "[CMD]: docker tag $PROJECT.api:$VERSION $PROFILE/$PROJECT.api:build"
 docker tag $PROJECT.api:$VERSION $PROFILE/$PROJECT.api:build
 
 # PUSH
 # cmd
-echo "[CMD]: docker push $PROFILE/$PROJECT.cmd:$VERSION"
 docker push $PROFILE/$PROJECT.cmd:$VERSION
 docker push $PROFILE/$PROJECT.cmd:build
 # api
-echo "[CMD]: docker push $PROFILE/$PROJECT.api:$VERSION"
 docker push $PROFILE/$PROJECT.api:$VERSION
 docker push $PROFILE/$PROJECT.api:build
 
-# finished
-echo "[INFO]: FINISHED BUILD ($PROFILE/$PROJECT:$VERSION)!"
+# docker build, tag, and push BUILD script finished
